647a8f86b6f4188c25a829ba9d5c59bbf819597f
Vladislav Belovitsky <vbelovitsky@yandex.ru>
2019-04-05 23:26:50 +0300

    add post layout

--- Mobile/SleepProject/app/src/main/java/com/andrewkir/sleepproject/Utilities/Post.kt
+++ Mobile/SleepProject/app/src/main/java/com/andrewkir/sleepproject/Utilities/Post.kt
@@ -5,11 +5,9 @@ class Post(var postId: String, var username: String, var displayName: String,
            var comments: ArrayList<Comment>, var viewers: ArrayList<String>, var views: Int, var isLiked: Boolean
 )
 
-class Comment(commenterId: String, commentBody: String, date: String){
-    var commenterId = commenterId
-    var commentBody = commentBody
-    var date = date
+class Comment(var commenterId: String, var commentBody: String, var date: String){
 }
 
-class PostMinified(var username: String, var body: String,var isLiked: Boolean,var likes: Int, var postId: String,var date: String, var comments:Int){
+class PostMinified(var username: String, var body: String,var isLiked: Boolean,var likes: Int, var postId: String,
+                   var date: String, var comments:Int){
 }
