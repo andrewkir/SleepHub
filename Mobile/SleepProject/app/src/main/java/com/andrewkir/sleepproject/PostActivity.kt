4bc29fe414e6d34a8332468966919fbafc3d4e13
Vladislav Belovitsky <vbelovitsky@yandex.ru>
2019-04-05 23:37:59 +0300

    add post values

--- Mobile/SleepProject/app/src/main/java/com/andrewkir/sleepproject/PostActivity.kt
+++ Mobile/SleepProject/app/src/main/java/com/andrewkir/sleepproject/PostActivity.kt
@@ -2,14 +2,33 @@ package com.andrewkir.sleepproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ShareCompat
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_post.*
+
 
 class PostActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
-    }
 
+        val postId = intent.getStringExtra("post_id")
+
+        val avatar = avatar
+        val username = username
+        val date = date
+        val postTitle = post_title
+        val body = body
+        val likes = likes
+        val comments = comments
+        val commentsList = comments_list
+        val edComment = comment_input
+        val sendComment = comment_send
+
+
+
+    }
 
 
 }
